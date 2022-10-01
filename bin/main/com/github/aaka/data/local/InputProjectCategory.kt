e232d9679c8b87927d21e5175f283bf7bb17d97a
bakharaalief <50726376+bakharaalief@users.noreply.github.com>
2022-10-01 23:14:14 +0700

    update in input_data.json and readme

---
+++ bin/main/com/github/aaka/data/local/InputProjectCategory.kt
@@ -0,0 +1,24 @@
+package com.github.aaka.data.local
+
+import com.squareup.moshi.JsonClass
+
+import com.squareup.moshi.Json
+
+
+@JsonClass(generateAdapter = true)
+data class InputProjectCategory(
+    @Json(name = "key")
+    val key: String, // $OTHER_REPOS
+    @Json(name = "pattern")
+    val pattern: String, // Other
+    @Json(name = "projects")
+    var inputProjects: List<InputProject>
+) {
+    @JsonClass(generateAdapter = true)
+    data class InputProject(
+        @Json(name = "github_url")
+        val githubUrl: String, // https://github.com/shadowsocks/shadowsocks-android
+        @Json(name = "stack")
+        val stack: String? // Testing, Room,  Firebase Ads, WorkManager
+    )
+}
